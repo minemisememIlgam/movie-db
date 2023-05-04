2a3a89c49f8aab882344960e1d79a49408a8b8c5
minemisememIlgam <127801046+minemisememIlgam@users.noreply.github.com>
2023-05-04 14:54:34 +0000

    searching

--- src/components/Films.js
+++ src/components/Films.js
@@ -2,12 +2,38 @@ import { useContext } from "react";
 import Context from "../Context";
 
 function Films() {
-  const { state, setState } = useContext(Context);
+  const { search, setSearch, state, setState } = useContext(Context);
 
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=c47574bca98aa7568df57f6ebd8f9886&query=${search}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setState(data.results);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function handleInputChange(event) {
+    setSearch(event.target.value);
+  }
+
+  console.log(search);
   return (
     <div className="movie">
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          value={search}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="search film"
+        />
+        <button type="submit">Search</button>
+      </form>
       {state ? (
-        state.map((elem) => {
+        state.slice(0, 10).map((elem) => {
           return (
             <div>
               <h1>{elem.title}</h1>
